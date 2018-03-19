8bb219ec5bd873908096c5549264ee7fb4035724
fveraMI <fvera@excelsis.com.py>
2018-03-19 17:04:57 -0300

    back adelantos

---
+++ modules/adelanto/adelanto.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var mongoosePaginate = require("mongoose-paginate");
+
+var Adelanto = new Schema(
+  {
+    fecha: {
+      type: Date,
+      required: true
+    },
+    funcionario: {
+      type: Schema.Types.ObjectId,
+      ref: "Funcionario"
+    },
+    tipoAdelanto: {
+      type: String,
+      required: true
+    },
+    moneda: {
+      type: String,
+      required: true
+    },
+    monto: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    collection: "adelantos"
+  }
+);
+Adelanto.plugin(mongoosePaginate);
+
+module.exports = mongoose.model("Adelanto", Adelanto);
