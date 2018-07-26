5d816304a8c6e076c05640a169b97fc5ecb077a6
fveraMI <fvera@excelsis.com.py>
2018-07-26 11:43:26 -0300

    ajustes

--- midlewares/authenticate.js
+++ midlewares/authenticate.js
@@ -1,19 +1,21 @@
-var Usuario = require("./../modules/usuario/usuario");
+var Usuario = require("./../modules/user/user");
 
-var authenticate = function(req, res, next){
-    var token = req.header('x-auth');
+var authenticate = function(req, res, next) {
+  var token = req.header("x-auth");
 
-     Usuario.findByToken(token).then((user)=>{
-       if(!user){
-          return Promise.reject();
-       }
-       console.log("User from findbytoken", req.user);
-       req.user = user;
-       req.token = token;
-       next();
-     }).catch((e)=>{
-       res.status(401).send();
-     })
-}
+  Usuario.findByToken(token)
+    .then(user => {
+      if (!user) {
+        return Promise.reject();
+      }
+      console.log("User from findbytoken", req.user);
+      req.user = user;
+      req.token = token;
+      next();
+    })
+    .catch(e => {
+      res.status(401).send();
+    });
+};
 
 module.exports = authenticate;
--- modules/user/index.js
+++ modules/user/index.js
@@ -1,36 +1,44 @@
 var express = require("express");
-var usuarioRoutes = express.Router();
+var userRoutes = express.Router();
 
 var User = require("./user");
 var auth = require("./../../midlewares/authenticate");
 
-userRoutes.route("/add").post(function (req, res) {
+userRoutes.route("/add").post(function(req, res) {
   var user = new User(req.body);
 
-  user.save().then(() => {
-    //res.status(200).json({ item: "Item added successfully" });
-    return user.generateAuthToken();
-  }).then((token) => {
-    res.header('x-auth', token).status(200).send(user);
-  })
+  user
+    .save()
+    .then(() => {
+      //res.status(200).json({ item: "Item added successfully" });
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      res
+        .header("x-auth", token)
+        .status(200)
+        .send(user);
+    })
     .catch(err => {
       res.status(400).send(err);
     });
-
 });
 
-userRoutes.route("/profile").get(auth, function (req, res) {
+userRoutes.route("/profile").get(auth, function(req, res) {
   res.send(req.user);
-})
+});
 
-userRoutes.route("/token").delete(auth, function (req, res) {
+userRoutes.route("/token").delete(auth, function(req, res) {
   console.log("User from delete token", req.user);
-  req.user.removeToken(req.token).then(() => {
-    res.status(200).send();
-  }, () => {
-    res.status(400).send();
-  })
-})
+  req.user.removeToken(req.token).then(
+    () => {
+      res.status(200).send();
+    },
+    () => {
+      res.status(400).send();
+    }
+  );
+});
 
 // userRoutes.get("/profile", auth, function(req, res){
 //   res.send(req.user);
