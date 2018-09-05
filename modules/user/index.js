ce81291de80ffd3b5708832eb28222370e0f5548
favera <faa88ver@gmail.com>
2018-09-05 19:26:03 -0400

    ajustes usuario

--- modules/user/index.js
+++ modules/user/index.js
@@ -4,7 +4,7 @@ var userRoutes = express.Router();
 var User = require("./user");
 var auth = require("./../../midlewares/authenticate");
 
-userRoutes.route("/add").post(function (req, res) {
+userRoutes.route("/add").post(function(req, res) {
   var user = new User(req.body);
 
   user
@@ -25,31 +25,38 @@ userRoutes.route("/add").post(function (req, res) {
 });
 
 //retorna el objeto a editar
-userRoutes.route("/edit/:id").get(function (req, res) {
-  User.findById(req.params.id, function (err, user) {
+userRoutes.route("/edit/:id").get(function(req, res) {
+  User.findById(req.params.id, function(err, user) {
     if (err) res.status(400).send(err);
-    res.status(200).send(user)
-  })
+    res.status(200).send(user);
+  });
+});
+
+// Elimina usuario
+userRoutes.route("/delete/:id").delete(function(req, res) {
+  User.findByIdAndRemove({ _id: req.params.id }, function(err, user) {
+    if (err) res.json(err);
+    else res.json("Successfully removed");
+  });
 });
 
 //Obtener listado de usuarios sin retornar contrasenha
-userRoutes.route("/users-list").get(function (req, res) {
+userRoutes.route("/users-list").get(function(req, res) {
   var query = User.find({});
   //excluye los campos password y tokens
-  query.select('-password -tokens');
+  query.select("-password -tokens");
 
-  query.exec(function (err, users) {
+  query.exec(function(err, users) {
     if (err) res.status(400).send(err);
-    res.status(200).send(users)
-  })
-
+    res.status(200).send(users);
+  });
 });
 
-userRoutes.route("/profile").get(auth, function (req, res) {
+userRoutes.route("/profile").get(auth, function(req, res) {
   res.send(req.user);
 });
 
-userRoutes.route("/token").delete(auth, function (req, res) {
+userRoutes.route("/token").delete(auth, function(req, res) {
   // console.log("User from delete token", req.user);
   req.user.removeToken(req.token).then(
     () => {
